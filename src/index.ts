67ce4120f494e27d1fb8e475929ec685145dd252
Erik Golinelli <erik@codekraft.it>
2025-11-22 17:07:22 +0100

    updated scripts initialization

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,24 @@
 /**
  * Initializes the MD4AI admin functionality on DOM load.
  */
-import {md4ai_markdown} from "./md4ai-markdown";
-import {md4aiCharts} from "./md4ai-charts";
-import {handleMd4aiButtons} from "./md4ai-admin";
-
+import { md4ai_markdown } from './md4ai-markdown';
+import { md4aiCharts } from './md4ai-charts';
+import { handleMd4aiButtons } from './md4ai-admin';
+import { initGeoInsights } from './md4ai-insights';
 
 document.addEventListener( 'DOMContentLoaded', () => {
-  const isAdminPage = document.querySelector( '.md4ai-admin' ) !== null;
-  const isMetabox = document.getElementById( 'md4ai_metabox' ) !== null;
+	const isAdminPage = document.querySelector( '.md4ai-admin' ) !== null;
+	const isMetabox = document.getElementById( 'md4ai_metabox' ) !== null;
 
-  if ( isAdminPage || isMetabox ) {
-    md4ai_markdown();
-    handleMd4aiButtons();
-  }
+	if ( isAdminPage || isMetabox ) {
+		md4ai_markdown();
+		handleMd4aiButtons();
+	}
 
-  const is_dashboard = document.querySelector( '.md4ai-charts-container' );
-  if ( is_dashboard ) {
-    md4aiCharts();
-  }
+	if ( document.querySelector( '.md4ai-charts-container' ) ) {
+		md4aiCharts();
+	}
+	if ( document.querySelector( '.geo-insights-wrapper' ) ) {
+		initGeoInsights();
+	}
 } );
